fae23be0ef7d7426c20507f5ffbeb5d9bf5306b9
deissibelle <deissibelle@gmail.com>
2025-03-14 16:11:37 +0100

    Final Touch

--- site/src/jsMain/kotlin/com/ds/sibelle/components/SectionTitle.kt
+++ site/src/jsMain/kotlin/com/ds/sibelle/components/SectionTitle.kt
@@ -1,17 +1,22 @@
 package com.ds.sibelle.components
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import com.ds.sibelle.models.Section
 import com.ds.sibelle.models.Theme
 import com.ds.sibelle.util.Constants.FONT_FAMILY
+import com.ds.sibelle.util.ObserveViewportEntered
 import com.varabyte.kobweb.compose.css.FontWeight
 import com.varabyte.kobweb.compose.css.TextAlign
+import com.varabyte.kobweb.compose.css.Transition
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.jetbrains.compose.web.css.ms
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
@@ -20,63 +25,85 @@ import org.jetbrains.compose.web.dom.Text
 fun SectionTitle(
     modifier: Modifier = Modifier,
     section: Section,
-    alignment: Alignment.Horizontal=Alignment.Start
-){
-  Column(
-      modifier =Modifier,
-      horizontalAlignment= alignment
-  ) {
-      P(
-          attrs = Modifier
+    alignment: Alignment.Horizontal = Alignment.Start
+) {
+    val scope = rememberCoroutineScope()
+    var titleMargin by remember { mutableStateOf(50.px) }
+    var subtitleMargin by remember { mutableStateOf(50.px) }
 
-              .fillMaxSize()
-              .textAlign(
-                  when (alignment) {
-                      Alignment.CenterHorizontally -> TextAlign.Center
-                      Alignment.End -> TextAlign.End
-                      else -> TextAlign.Start
-                  }
-              )
-              .margin (topBottom = 0.px)
-              .fontFamily(FONT_FAMILY)
-              .fontSize(25.px)
-              .fontWeight(FontWeight.Normal)
-              .color(Theme.Primary.rgb)
+    ObserveViewportEntered(
+        sectionId = section.id,
+        distanceFromTop = 700.0,
+        onViewportEntered = {
+            scope.launch {
+                subtitleMargin = 0.px
+                if (alignment == Alignment.Start) {
+                    delay(25)
+                }
+                titleMargin = 0.px
+            }
+        }
+    )
 
-              .toAttrs()
-      ){
-          Text(section.title)
-
-      }
-      P(
-          attrs = Modifier
-
-              .fillMaxSize()
-              .textAlign(
-                  when (alignment) {
-                      Alignment.CenterHorizontally -> TextAlign.Center
-                      Alignment.End -> TextAlign.End
-                      else -> TextAlign.Start
-                  }
-              )
-              .margin (bottom = 10.px, top = 0.px)
-              .fontFamily(FONT_FAMILY)
-              .fontSize(40.px)
-              .fontWeight(FontWeight.Bold)
-              .color(Theme.Secondary.rgb)
-
-              .toAttrs()
-      ){
-          Text(section.subtitle)
-
-      }
-       Box (
-           modifier = Modifier.height(2.px)
-               .width(80.px)
-               .backgroundColor(Theme.Primary.rgb)
-               .borderRadius (r=50.px)
-       ){
-
-       }
-  }
+    Column(
+        modifier = modifier,
+        horizontalAlignment = alignment
+    ) {
+        P(
+            attrs = Modifier
+                .fillMaxWidth()
+                .textAlign(
+                    when (alignment) {
+                        Alignment.CenterHorizontally -> TextAlign.Center
+                        Alignment.End -> TextAlign.End
+                        else -> TextAlign.Start
+                    }
+                )
+                .margin(
+                    left = titleMargin,
+                    top = 0.px,
+                    bottom = 0.px
+                )
+                .fontFamily(FONT_FAMILY)
+                .fontSize(25.px)
+                .fontWeight(FontWeight.Normal)
+                .color(Theme.Primary.rgb)
+                .transition(Transition.of(property = "margin", duration = 300.ms))
+                .toAttrs()
+        ) {
+            Text(section.title)
+        }
+        P(
+            attrs = Modifier
+                .fillMaxWidth()
+                .textAlign(
+                    when (alignment) {
+                        Alignment.CenterHorizontally -> TextAlign.Center
+                        Alignment.End -> TextAlign.End
+                        else -> TextAlign.Start
+                    }
+                )
+                .margin(
+                    left = if(alignment == Alignment.Start) subtitleMargin else 0.px,
+                    right = if(alignment == Alignment.CenterHorizontally) subtitleMargin else 0.px,
+                    bottom = 10.px,
+                    top = 0.px
+                )
+                .fontFamily(FONT_FAMILY)
+                .fontSize(40.px)
+                .fontWeight(FontWeight.Bold)
+                .color(Theme.Secondary.rgb)
+                .transition(Transition.of(property = "margin", duration = 300.ms))
+                .toAttrs()
+        ) {
+            Text(section.subtitle)
+        }
+        Box(
+            modifier = Modifier
+                .height(2.px)
+                .width(80.px)
+                .backgroundColor(Theme.Primary.rgb)
+                .borderRadius(r = 50.px)
+        )
+    }
 }
